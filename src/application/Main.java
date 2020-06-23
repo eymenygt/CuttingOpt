23236c904556a49700fcad161d9d4e209ff6eead
Bora Aydın <47326422+ytg32@users.noreply.github.com>
2020-06-23 20:08:10 +0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package src.application;
 	
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.collections.FXCollections;
@@ -15,16 +16,24 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
+//        			............. TODO............              		//
+//        	make table componentes(length,quantity) non-moveable        //
+//        	create another canvas for optimization procces    			//
+//        	print error msg for negative values               			//
 
 
 public class Main extends Application{
 
     Stage window;
     TableView<Product> table;
-    TextField lengthText, quantitiyText;
-    Button addButton,deleteButton;
+    TextField lengthText, quantitiyText, profileLengthtext;
+    Button addButton,deleteButton,optimizeButton;
+    TableColumn<Product,Double> lengthColumn;
+    TableColumn<Product,Double> quantityColumn;
     
     public static void main(String[] args){
         launch(args);
@@ -36,12 +45,12 @@ public class Main extends Application{
         window.setTitle("Deneme");
        
         //length column
-        TableColumn<Product,Double> lengthColumn = new TableColumn<>("Length");
+        lengthColumn = new TableColumn<>("Length");
         lengthColumn.setMinWidth(200);
         lengthColumn.setCellValueFactory(new PropertyValueFactory<>("length"));
 
         //Quantity column
-        TableColumn<Product,Double> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn = new TableColumn<>("Quantity");
         quantityColumn.setMinWidth(200);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
         
@@ -53,11 +62,17 @@ public class Main extends Application{
         quantitiyText = new TextField();
         quantitiyText.setPromptText("QUANTITY");
         
+        profileLengthtext = new TextField();
+        profileLengthtext.setPromptText("PROFILE LENGTH");
+        
         //Buttons
         addButton = new Button("ADD");
         addButton.setOnAction(e -> addButtonHandler());
         deleteButton = new Button("DELETE");
         deleteButton.setOnAction(e -> deleteButtonHandler());
+        optimizeButton = new Button("OPTIMIZE");
+        optimizeButton.setOnAction(e -> optimizeButtonHandler());
+        
         
         //layout for addButton deleteButton...
         HBox hbox = new HBox();
@@ -65,15 +80,25 @@ public class Main extends Application{
         hbox.setSpacing(10);
         hbox.getChildren().addAll(lengthText,quantitiyText,addButton,deleteButton);
         
+        Region region1 = new Region();
+        HBox.setHgrow(region1, Priority.ALWAYS);
+        
+        HBox hBox2 = new HBox();
+        hBox2.setPadding(new Insets(0,10,10,10));
+        hBox2.setSpacing(10);
+        hBox2.getChildren().addAll(region1,profileLengthtext,optimizeButton);
+        
         table = new TableView<>();
+        table.setEditable(true);
         table.setItems(getProduct());
         table.getColumns().addAll(lengthColumn,quantityColumn);
 
 
 
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(table,hbox);
+        vBox.getChildren().addAll(table,hbox,hBox2);
 
+        
         Scene scene = new Scene(vBox);
         window.setScene(scene);
         window.show();
@@ -83,10 +108,30 @@ public class Main extends Application{
         try {
             double length = Double.parseDouble(lengthText.getText());
             int quantity = Integer.parseInt(quantitiyText.getText());
-            Product p = new Product(length,quantity);
-            table.getItems().add(p);
             lengthText.clear();
             quantitiyText.clear();
+            
+            //if length or quanitity vlaues are negative
+            if(length <= 0 || quantity <= 0) return;
+            //          ............. TODO............         //
+            //        print error msg for negative values      //
+            
+            ArrayList<String> t = getTableViewColumn(table,0);
+            //if length value doesn't exist in table
+            
+            if(!t.contains(String.valueOf(length))) {
+                Product p = new Product(length,quantity);
+                table.getItems().add(p);
+            }
+            //if length value exists in table
+            else {
+                int index = t.indexOf(String.valueOf(length));
+                Product oldP = table.getItems().get(index);
+                int oldQ = oldP.getQuantity();
+                Product p = new Product(length,oldQ + quantity);
+                table.getItems().set(index,p);
+            }
+        
         }catch(NumberFormatException e) {
             // invalid inputs will be ignored.
         }    
@@ -96,6 +141,20 @@ public class Main extends Application{
         Product selectedItem = table.getSelectionModel().getSelectedItem();
         table.getItems().remove(selectedItem);
     }
+    
+    public void optimizeButtonHandler() {
+        //      ............. TODO............         //
+    }
+    
+    // function to get the values of desired column
+    private static ArrayList<String> getTableViewColumn(TableView tableView,int idx) {
+        ArrayList<String> values = new ArrayList<>();
+        ObservableList<TableColumn> columns = tableView.getColumns();
+        for (Object row : tableView.getItems()) {
+                values.add((columns.get(idx).getCellObservableValue(row).getValue().toString()));
+        }
+        return values;
+}
     
     public ObservableList<Product> getProduct() {
         ObservableList<Product> products = FXCollections.observableArrayList();
